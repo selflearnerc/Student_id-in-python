4132f033e8658ff6dd5701cef13a7b0f8e861e11
selflearnerc <skye90480@gmail.com>
2025-08-04 03:27:29 -0700

    Add files via upload

---
+++ base.py
@@ -0,0 +1,20 @@
+name = "shahzad"
+age = int("18")
+height = float("4.9")
+language = "python"
+is_learning_with_python = True
+print(f"my name is {name.capitalize()}. I am {age} years old  I am learning {language}: {is_learning_with_python}")
+
+
+
+name = input("enter your name: ")
+age = input("enter your age: ")
+
+print("hey", name, "you are", age, "years old")
+
+name = input("your name: ")
+age = int(input("your age: "))
+
+next = age + 1
+
+print("Hey", name, "next year you will be", next, "years old")
